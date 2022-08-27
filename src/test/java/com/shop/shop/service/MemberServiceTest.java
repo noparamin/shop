4c0612f0202ee20d37f44c90772160c3d782ef41
noparamin <noparamin@naver.com>
2022-08-27 16:52:22 +0900

    회원가입 구현, 테스트 작성

---
+++ src/test/java/com/shop/shop/service/MemberServiceTest.java
@@ -0,0 +1,64 @@
+package com.shop.shop.service;
+
+import com.shop.shop.dto.MemberFormDto;
+import com.shop.shop.entity.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.yml")
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    public Member createMember(){
+        MemberFormDto memberFormDto = new MemberFormDto();
+        memberFormDto.setEmail("test@naver.com");
+        memberFormDto.setName("홍길동");
+        memberFormDto.setAddress("서울시 송파구 잠실동");
+        memberFormDto.setPassword("1234");
+
+        return Member.createMember(memberFormDto, passwordEncoder);
+    }
+
+    @Test
+    @DisplayName("회원가입 테스트")
+    public void saveMemberTest() {
+        // given
+        Member member = createMember();
+
+        // when
+        Member savedMember = memberService.saveMember(member);
+
+        // then
+        Assertions.assertThat(member).isEqualTo(savedMember);
+    }
+
+    @Test
+    @DisplayName("중복 회원가입 테스트")
+    public void saveDuplicateMemberTest() {
+        Member member1 = createMember();
+        Member member2 = createMember();
+        memberService.saveMember(member1);
+
+        Throwable e = assertThrows(IllegalStateException.class, () -> {
+            memberService.saveMember(member2);
+        });
+
+        assertEquals("이미 가입된 회원입니다.", e.getMessage());
+    }
+
+}
