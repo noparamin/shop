fd69c91e6e3b10bf59dad25ad37cacf1f24ae479
noparamin <noparamin@naver.com>
2022-08-29 16:12:51 +0900

    SecurityConfig 로그인 설정, MemberService UserDetailsService 구현

--- src/main/java/com/shop/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/shop/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -15,7 +16,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authorization) -> authorization.anyRequest().permitAll()).httpBasic(Customizer.withDefaults());
+        http.formLogin()
+                .loginPage("/members/login")
+                .defaultSuccessUrl("/")
+                .usernameParameter("email")
+                .failureUrl("/members/login/error")
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
+                .logoutSuccessUrl("/");
+        //http.authorizeHttpRequests((authorization) -> authorization.anyRequest().permitAll()).httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
