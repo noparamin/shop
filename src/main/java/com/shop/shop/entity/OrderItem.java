104e277b3e70b3d3018d17744d5d5c07380ba9b4
noparamin <noparamin@naver.com>
2022-09-08 21:25:28 +0900

    엔티티 BaseEntity 상속받도록 수정

--- src/main/java/com/shop/shop/entity/OrderItem.java
+++ src/main/java/com/shop/shop/entity/OrderItem.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Getter @Setter
-public class OrderItem {
+public class OrderItem extends BaseEntity {
 
     @Id
     @GeneratedValue
@@ -27,7 +27,7 @@ public class OrderItem {
 
     private int count;
 
-    private LocalDateTime regTime;
+    //private LocalDateTime regTime;
 
-    private LocalDateTime updateTime;
+    //private LocalDateTime updateTime;
 }
