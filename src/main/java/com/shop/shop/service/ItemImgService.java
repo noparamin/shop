ceb15fb851cf51d43aa62907170271786fa016fc
noparamin <noparamin@naver.com>
2022-09-13 20:19:37 +0900

    ItemImgService 구현

---
+++ src/main/java/com/shop/shop/service/ItemImgService.java
@@ -0,0 +1,37 @@
+package com.shop.shop.service;
+
+import com.shop.shop.entity.ItemImg;
+import com.shop.shop.repository.ItemImgRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import org.thymeleaf.util.StringUtils;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ItemImgService {
+
+    @Value("${itemImgLocation}")
+    private String itemImgLocation;
+
+    private final ItemImgRepository itemImgRepository;
+
+    private final FileService fileService;
+
+    public void saveItemImg(ItemImg itemImg, MultipartFile itemImgFile) throws Exception {
+        String oriImgName = itemImgFile.getOriginalFilename();
+        String imgName = "";
+        String imgUrl = "";
+
+        if(!StringUtils.isEmpty(oriImgName)) {
+            imgName = fileService.uploadFile(itemImgLocation, oriImgName, itemImgFile.getBytes());
+            imgUrl = "/images/item" + itemImg;
+        }
+
+        itemImg.updateItemImg(oriImgName, imgName, imgUrl);
+        itemImgRepository.save(itemImg);
+    }
+}
