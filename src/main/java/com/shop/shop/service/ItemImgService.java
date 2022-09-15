a8607679e1a78138e5242647cb7247f5f8252ff2
noparamin <noparamin@naver.com>
2022-09-15 20:54:50 +0900

    ItemImgService saveItemImg 메서드 로직 수정

--- src/main/java/com/shop/shop/service/ItemImgService.java
+++ src/main/java/com/shop/shop/service/ItemImgService.java
@@ -3,6 +3,7 @@ package com.shop.shop.service;
 import com.shop.shop.entity.ItemImg;
 import com.shop.shop.repository.ItemImgRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,9 +30,10 @@ public class ItemImgService {
         if(!StringUtils.isEmpty(oriImgName)) {
             imgName = fileService.uploadFile(itemImgLocation, oriImgName, itemImgFile.getBytes());
             imgUrl = "/images/item" + itemImg;
+
+            itemImg.updateItemImg(oriImgName, imgName, imgUrl);
+            itemImgRepository.save(itemImg);
         }
 
-        itemImg.updateItemImg(oriImgName, imgName, imgUrl);
-        itemImgRepository.save(itemImg);
     }
 }
